d96dcae09c9446bfae33fa419463cfc737c81988
Adam-Student12 <113127879+Adam-Student12@users.noreply.github.com>
2022-10-09 16:45:19 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+let navbar = document.getElementById("navbar");
+let navicon = document.getElementById("navicon");
+
+navbar.onclick = function (e) {
+    if () {
+        
+    } else if () {
+        
+    } else {
+        
+    }
+}
+navicon.onclick = function (e) {
+    if () {
+        
+    } else if () {
+        
+    } else {
+        
+    }
+}
